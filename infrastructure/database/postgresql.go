6429ffb3233f55c4b0253916b05815ed53b075b1
zaiduszh <zaidus.zhuhur@sprintasia.co.id>
2025-09-13 01:17:22 +0700

    refactor db connection

--- infrastructure/database/postgresql.go
+++ infrastructure/database/postgresql.go
@@ -12,12 +12,17 @@ import (
 )
 
 func ConnDb() (db *gorm.DB, sqlDB *sql.DB, err error) {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta",
-		utils.GetEnv("DB_HOST", "").(string),
-		utils.GetEnv("DB_PORT", "").(string),
-		utils.GetEnv("DB_USERNAME", "").(string),
-		utils.GetEnv("DB_PASS", "").(string),
-		utils.GetEnv("DB_NAME", "").(string))
+	dsn := utils.GetEnv("DATABASE_URL", "").(string)
+
+	if dsn == "" {
+		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Jakarta",
+			utils.GetEnv("DB_HOST", "").(string),
+			utils.GetEnv("DB_PORT", "").(string),
+			utils.GetEnv("DB_USERNAME", "").(string),
+			utils.GetEnv("DB_PASS", "").(string),
+			utils.GetEnv("DB_NAME", "").(string),
+			utils.GetEnv("DB_SSLMODE", "disable").(string))
+	}
 	logger.WriteLog(logger.LogLevelDebug, fmt.Sprintf("ConnDb; Initialize db connection..."))
 
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{TranslateError: true})
